32372d98a935423d4fccb7a1328ab84d57c53f4f
ambitiouschild <ambitiouschild@qq.com>
2025-06-05 20:27:03 +0800

    【公共模块打包】公共模块打包【0.1】

--- aioveu-oms/oms-boot/src/main/java/com/aioveu/oms/listener/OmsCloseListener.java
+++ aioveu-oms/oms-boot/src/main/java/com/aioveu/oms/listener/OmsCloseListener.java
@@ -6,10 +6,7 @@ import com.aioveu.oms.service.app.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.annotation.*;
 
 import java.io.IOException;
 
