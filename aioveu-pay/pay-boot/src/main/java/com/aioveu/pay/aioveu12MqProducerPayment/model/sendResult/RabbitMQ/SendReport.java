b2b577b81cbcef9d82defcae192ef384f4001c9f
ambitiouschild <ambitiouschild@qq.com>
2026-05-14 17:27:06 +0800

    【Pay】Mq修复SendReport发送报告类【31.1】

--- aioveu-pay/pay-boot/src/main/java/com/aioveu/pay/aioveu12MqProducerPayment/model/sendResult/RabbitMQ/SendReport.java
+++ aioveu-pay/pay-boot/src/main/java/com/aioveu/pay/aioveu12MqProducerPayment/model/sendResult/RabbitMQ/SendReport.java
@@ -9,10 +9,11 @@ import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @ClassName: SendReport
- * @Description TODO
+ * @Description TODO 发送报告类
  * @Author aioveu
  * @Author 雒世松
  * @Date 2026/5/14 17:10
@@ -34,11 +35,36 @@ public class SendReport {
 
     public String toSummary() {
         return String.format(
-                "发送报告: 总数=%d, 成功=%d(%.2f%%), 平均耗时=%dms, 最慢=%dms, 失败分布[超时=%d,路由失败=%d,NACK=%d,其他=%d]",
-                totalResults, stats.successCount, stats.successRate, stats.avgCostTime,
-                stats.maxCostTime, stats.timeoutCount, stats.routingFailedCount,
-                stats.nackCount, stats.otherFailureCount
+                "发送报告: 总数=%d, 成功=%d(%.2f%%), 平均耗时=%dms, 最慢=%dms\n" +
+                        "失败详情: 超时=%d, 路由失败=%d, NACK=%d, 其他=%d\n" +
+                        "最慢的%d个消息: %s\n" +
+                        "失败的%d个消息: %s",
+                totalResults,
+                stats.getSuccessCount(),
+                stats.getSuccessRate(),
+                stats.getAvgCostTime(),
+                stats.getMaxCostTime(),
+                stats.getTimeoutCount(),
+                stats.getRoutingFailedCount(),
+                stats.getNackCount(),
+                stats.getOtherFailureCount(),
+                slowestMessages.size(),
+                formatMessageIds(slowestMessages),
+                failedMessages.size(),
+                formatMessageIds(failedMessages)
         );
+    }
+
+    private String formatMessageIds(List<RabbitSendResult> results) {
+        if (results.isEmpty()) {
+            return "无";
+        }
+        return results.stream()
+                .limit(5)  // 只显示前5个
+                .map(r -> String.format("%s(%dms)",
+                        r.getMessageId(), r.getCostTime()))
+                .collect(Collectors.joining(", "))
+                + (results.size() > 5 ? String.format(" 等%d个...", results.size()) : "");
     }
 
 
