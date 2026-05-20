850f5685d3f8326ec835ab2836b8c624fcda66ca
ambitiouschild <ambitiouschild@qq.com>
2026-05-20 21:51:52 +0800

    【common-rabbitmq】实现中间件抽取2【36.9】

---
+++ aioveu-common/common-rocketmq/src/main/java/com/aioveu/rocketmq/producer/service/RocketMessageService.java
@@ -0,0 +1,19 @@
+package com.aioveu.rocketmq.producer.service;
+
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName: MqMessageService
+ * @Description TODO MQ消息服务类
+ * @Author aioveu
+ * @Author 雒世松
+ * @Date 2026/5/11 19:00
+ * @Version 1.0
+ **/
+
+public interface RocketMessageService {
+
+
+}
