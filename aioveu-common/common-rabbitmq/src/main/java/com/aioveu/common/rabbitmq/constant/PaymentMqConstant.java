a49416247c91438ed4209be70fd7fec716989f2d
ambitiouschild <ambitiouschild@qq.com>
2026-05-28 18:34:00 +0800

    【com-rabbit】 SYSTEM_NAME + Queue【39.6】

--- aioveu-common/common-rabbitmq/src/main/java/com/aioveu/common/rabbitmq/constant/PaymentMqConstant.java
+++ aioveu-common/common-rabbitmq/src/main/java/com/aioveu/common/rabbitmq/constant/PaymentMqConstant.java
@@ -16,7 +16,7 @@ public interface PaymentMqConstant {
 
     //把常量分组（一眼看出用途）
     // 1️补一个“消费者组 / 环境前缀”（防脏数据）
-    String SYSTEM_NAME = "oms";
+    String SYSTEM_NAME = "oms.";
 
     interface Exchange {
         String PAYMENT = "payment.exchange";
@@ -25,8 +25,8 @@ public interface PaymentMqConstant {
 
     interface Queue {
         String SUCCESS = SYSTEM_NAME + "payment.success.queue";
-        String FAILED  = "payment.failed.queue";
-        String DLQ     = "payment.success.queue.dlq";
+        String FAILED  = SYSTEM_NAME + "payment.failed.queue";
+        String DLQ     = SYSTEM_NAME + "payment.success.queue.dlq";
     }
 
     interface RoutingKey {
